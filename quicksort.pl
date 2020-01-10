9180f3181973b84d86e4c8697971dc4271c2d214
kubamarc <kubamarc@gmail.com>
2020-01-10 11:48:01 +0100

    Dodany warunek brzegowy, dla pustych tablic

--- quicksort.pl
+++ quicksort.pl
@@ -1,6 +1,8 @@
 % Implementacja Quicksorta w Prologu
 % Autor korpo, https://github.com/korpo
 
+
+project_dedalus_quicksort([],[]).
 project_dedalus_quicksort([X],[X]).
 project_dedalus_quicksort([X|Xs],Ys) :-
   project_dedalus_quicksort_helper_partition(Xs,X,Left,Right),
@@ -8,10 +10,10 @@ project_dedalus_quicksort([X|Xs],Ys) :-
   project_dedalus_quicksort(Right,Rs),
   append(Ls,[X|Rs],Ys).
 
-project_dedalus_quicksort_helper_partition([X|Xs],Y,[X|Ls],Rs) :- 
-  X =< Y, 
+project_dedalus_quicksort_helper_partition([X|Xs],Y,[X|Ls],Rs) :-
+  X =< Y,
   project_dedalus_quicksort_helper_partition(Xs,Y,Ls,Rs).
-project_dedalus_quicksort_helper_partition([X|Xs],Y,Ls,[X|Rs]) :- 
-  X  > Y, 
+project_dedalus_quicksort_helper_partition([X|Xs],Y,Ls,[X|Rs]) :-
+  X  > Y,
   project_dedalus_quicksort_helper_partition(Xs,Y,Ls,Rs).
 project_dedalus_quicksort_helper_partition([],_,[],[]).
